53d79b675513b9ff81fbfdf19d8fbb96c3d2bfaa
Tamires Santiago <tamireszsantiago@gmail.com>
2025-09-03 16:10:57 -0300

    add: dificuldade no arquivo de mapa médio

--- data/maps/medio.pl
+++ data/maps/medio.pl
@@ -15,7 +15,8 @@ maps([
           [vazio,vazio,vazio,vazio,parede,vazio,vazio,vazio,vazio,vazio,parede,parede,parede,parede,parede,parede,parede],
           [vazio,vazio,vazio,vazio,parede,parede,parede,parede,parede,parede,parede,vazio,vazio,vazio,vazio,vazio,vazio]
         ],
-        [[6,16],[6,17],[7,16],[7,17],[8,16],[8,17]]
+        [[6,16],[6,17],[7,16],[7,17],[8,16],[8,17]],
+        medio
     ),
 
     map(
@@ -30,7 +31,8 @@ maps([
           [vazio,vazio,parede,vazio,vazio,vazio,parede,vazio,vazio,vazio,vazio,parede],
           [vazio,vazio,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede]
         ],
-        [[1,1],[1,2],[2,1],[2,2],[3,1],[3,2],[4,1],[4,2],[5,1],[5,2]]
+        [[1,1],[1,2],[2,1],[2,2],[3,1],[3,2],[4,1],[4,2],[5,1],[5,2]],
+        medio
     ),
 
     map(
@@ -45,7 +47,8 @@ maps([
           [parede,marca,marca,marca,vazio,vazio,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede],
           [parede,parede,parede,parede,parede,parede,parede,parede,vazio,vazio,vazio,vazio,vazio,vazio,vazio]
         ],
-        [[6,1],[6,2],[6,3],[6,4],[7,2],[7,3],[7,4],[8,1],[8,2],[8,3],[8,4]]
+        [[6,1],[6,2],[6,3],[6,4],[7,2],[7,3],[7,4],[8,1],[8,2],[8,3],[8,4]],
+        medio
     ),
 
     map(
@@ -64,7 +67,8 @@ maps([
           [parede,vazio,vazio,vazio,parede,vazio,vazio,vazio,parede,parede,vazio,vazio,vazio,vazio,vazio],
           [parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,vazio,vazio,vazio,vazio]
         ],
-        [[1,14],[1,15],[1,16],[1,17],[2,14],[2,15],[2,16],[2,17],[3,14],[3,15],[3,16],[3,17],[4,14],[4,15],[4,16],[4,17],[5,14],[5,15],[5,16],[5,17]]
+        [[1,14],[1,15],[1,16],[1,17],[2,14],[2,15],[2,16],[2,17],[3,14],[3,15],[3,16],[3,17],[4,14],[4,15],[4,16],[4,17],[5,14],[5,15],[5,16],[5,17]],
+        medio
     ),
 
     map(
@@ -82,7 +86,8 @@ maps([
           [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,vazio,vazio,parede],
           [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,parede,parede]
         ],
-        [[5,1],[5,2],[5,3],[5,4],[6,1],[6,2],[6,3],[6,4],[7,1],[7,2],[7,3],[7,4]]
+        [[5,1],[5,2],[5,3],[5,4],[6,1],[6,2],[6,3],[6,4],[7,1],[7,2],[7,3],[7,4]],
+        medio
     )
 
 ]).
