3235371b3d152017581f51d7415a86dba67077ae
Eduarda Viana <83471555+eduardaviana@users.noreply.github.com>
2025-09-12 21:43:05 -0300

    fix: mapa dificil

--- data/maps/dificil.pl
+++ data/maps/dificil.pl
@@ -6,15 +6,15 @@ mapas([
         [
           [vazio,vazio,vazio,parede,parede,parede,vazio,vazio,vazio,vazio,vazio],
           [vazio,vazio,vazio,parede,vazio,parede,vazio,parede,parede,parede,parede],
-          [vazio,vazio,parede,vazio,vazio,parede,parede,parede,vazio,vazio,parede],
-          [vazio,parede,vazio,caixa,vazio,vazio,vazio,vazio,vazio,vazio,parede],
-          [parede,vazio,vazio,vazio,jogador,caixa,vazio,parede,vazio,vazio,parede],
-          [vazio,parede,parede,vazio,caixa,parede,parede,parede,vazio,vazio,parede],
-          [vazio,vazio,parede,vazio,vazio,parede,marca,marca,vazio,vazio,parede],
-          [vazio,vazio,parede,vazio,parede,parede,marca,parede,vazio,parede,vazio],
+          [vazio,vazio,parede,vazio,vazio,parede,parede,parede,vazio,vazio,parede], 
+          [vazio,parede,vazio,caixa,vazio,vazio,vazio,vazio,vazio,vazio,parede], 
+          [parede,vazio,vazio,vazio,jogador,caixa,vazio,parede,vazio,vazio,parede], 
+          [vazio,parede,parede,vazio,caixa,parede,parede,parede,vazio,vazio,parede], 
+          [vazio,vazio,parede,vazio,vazio,parede,marca,marca,vazio,vazio,parede], 
+          [vazio,vazio,parede,vazio,parede,parede,marca,parede,vazio,parede,vazio], 
           [vazio,parede,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio],
           [vazio,parede,vazio,vazio,vazio,vazio,vazio,parede,vazio,vazio,vazio],
-          [vazio,parede,parede,parede,parede,parede,parede,vazio,vazio,vazio]
+          [vazio,parede,parede,parede,parede,parede,parede,parede,vazio,vazio,vazio]
         ],
         [[6,6],[6,7],[7,6]],
         dificil
@@ -55,19 +55,19 @@ mapas([
         [
           [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,parede,parede,parede,vazio,vazio,vazio],
           [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,vazio,parede,parede,vazio,vazio],
-          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,vazio,vazio,parede,vazio,vazio],
-          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,caixa,caixa,vazio,parede,vazio,vazio],
-          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,caixa,caixa,vazio,caixa,vazio,parede,vazio,vazio],
-          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,vazio,caixa,vazio,vazio,vazio,caixa,vazio,parede,vazio,vazio],
-          [parede,parede,parede,parede,vazio,vazio,vazio,parede,vazio,vazio,vazio,caixa,caixa,vazio,parede,parede,parede,parede,parede],
-          [parede,vazio,vazio,parede,parede,parede,parede,parede,parede,parede,parede,vazio,parede,parede,vazio,vazio,vazio,parede],
-          [parede,marca,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,caixa,caixa,caixa,jogador,parede],
-          [parede,marca,parede,vazio,parede,parede,parede,parede,parede,parede,parede,vazio,parede,parede,vazio,vazio,parede,parede],
-          [parede,marca,parede,vazio,parede,parede,parede,parede,parede,parede,marca,vazio,parede,caixa,vazio,caixa,parede,parede],
-          [parede,marca,marca,marca,marca,marca,marca,marca,marca,marca,marca,vazio,parede,vazio,vazio,vazio,vazio,parede],
-          [parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,vazio,vazio,caixa,vazio,parede],
-          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,parede,parede],
-          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,parede,parede,vazio]
+          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,vazio,vazio,vazio,parede,vazio,vazio],
+          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,caixa,caixa,vazio,vazio,parede,vazio,vazio], 
+          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,caixa,caixa,vazio,vazio,caixa,vazio,parede,vazio,vazio],
+          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,vazio,caixa,vazio,vazio,vazio,vazio,caixa,vazio,parede,vazio,vazio], 
+          [parede,parede,parede,parede,vazio,vazio,vazio,parede,vazio,vazio,vazio,caixa,caixa,vazio,parede,parede,parede,parede,parede], 
+          [parede,vazio,vazio,parede,parede,parede,parede,parede,parede,parede,parede,vazio,parede,parede,vazio,vazio,vazio,vazio,parede], 
+          [parede,marca,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,caixa,caixa,caixa,jogador,parede], 
+          [parede,marca,parede,vazio,parede,parede,parede,parede,parede,parede,parede,vazio,parede,parede,vazio,vazio,vazio,parede,parede], 
+          [parede,marca,parede,vazio,parede,parede,parede,parede,parede,parede,parede,marca,vazio,parede,caixa,vazio,caixa,parede,parede], 
+          [parede,marca,marca,marca,marca,marca,marca,marca,marca,marca,marca,marca,vazio,parede,vazio,vazio,vazio,vazio,parede], 
+          [parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,parede,vazio,vazio,caixa,vazio,parede],
+          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,vazio,vazio,parede,parede],
+          [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio,parede,parede,parede,parede,vazio]
         ],
         [[8,1],[9,1],[10,1],[10,11],[11,1],[11,2],[11,3],[11,4],[11,5],[11,6],[11,7],[11,8],[11,9],[11,10],[11,11]],
         dificil
@@ -75,19 +75,19 @@ mapas([
 
   mapa(
         [
-          [vazio,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,vazio,vazio],
-          [vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,vazio],
+          [vazio,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,vazio],
+          [vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio],
           [parede,vazio,vazio,vazio,marca,caixa,vazio,vazio,vazio,vazio,marca,vazio,caixa,marca,vazio,vazio,marca,vazio,caixa,vazio,vazio,vazio,marca,caixa,vazio,vazio,parede],
-          [vazio,parede,vazio,caixa,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,vazio,vazio,parede,vazio],
-          [parede,vazio,vazio,marca,vazio,vazio,marca,vazio,vazio,vazio,vazio,caixa,marca,vazio,vazio,marca,vazio,caixa,marca,vazio,vazio,marca,vazio,parede],
-          [vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,vazio,parede,caixa,parede,caixa,vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio],
-          [parede,vazio,marca,vazio,vazio,vazio,vazio,vazio,marca,caixa,vazio,marca,caixa,vazio,vazio,vazio,vazio,marca,vazio,caixa,marca,vazio,vazio,marca,vazio,vazio,parede],
-          [vazio,parede,vazio,caixa,parede,vazio,parede,vazio,parede,vazio,parede,jogador,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,caixa,vazio,parede,vazio],
-          [parede,vazio,vazio,marca,vazio,vazio,marca,caixa,vazio,marca,vazio,vazio,vazio,vazio,caixa,marca,vazio,caixa,vazio,vazio,vazio,vazio,marca,vazio,parede],
-          [vazio,parede,vazio,parede,caixa,parede,vazio,parede,caixa,vazio,caixa,parede,caixa,parede,vazio,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio],
-          [parede,vazio,marca,vazio,vazio,marca,vazio,caixa,marca,vazio,vazio,marca,vazio,vazio,caixa,vazio,vazio,vazio,marca,vazio,vazio,marca,vazio,vazio,parede],
-          [vazio,parede,vazio,vazio,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,caixa,vazio,parede,vazio],
-          [parede,vazio,vazio,caixa,marca,vazio,vazio,vazio,caixa,vazio,marca,vazio,vazio,caixa,marca,vazio,vazio,vazio,caixa,marca,vazio,vazio,vazio,parede],
+          [vazio,parede,vazio,caixa,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,vazio,parede,vazio,vazio,parede,vazio], 
+          [parede, vazio, vazio, marca, vazio, vazio, marca, vazio, vazio, vazio,vazio, caixa, marca, vazio, vazio, marca, vazio, vazio, marca, caixa, vazio, marca, vazio, vazio, marca, vazio, parede], 
+          [vazio, parede, vazio, parede, caixa, parede, vazio, parede, vazio, parede, vazio, parede, vazio, parede, caixa, parede, caixa, vazio, caixa, parede, vazio, parede, caixa, parede, vazio, parede, vazio], 
+          [parede, vazio, marca, vazio, vazio, vazio, vazio, vazio, marca, caixa, vazio, marca, caixa, vazio, vazio, vazio, vazio, marca, vazio, caixa, marca, vazio, vazio, marca, vazio, vazio, parede], 
+          [vazio, parede, vazio, caixa, parede, vazio, parede, vazio, parede, vazio, parede, vazio, parede, jogador, parede, vazio, parede, vazio, parede, vazio, parede, vazio, parede, caixa, vazio, parede, vazio], 
+          [parede, vazio, vazio, marca, vazio, vazio, marca, caixa, vazio, marca, vazio, vazio, vazio, vazio, caixa, marca, vazio, caixa, marca, vazio, vazio, vazio, vazio, vazio, marca, vazio, parede], 
+          [vazio, parede, vazio, parede, caixa, parede, vazio, parede, caixa, vazio, caixa, parede, caixa, parede, vazio, parede, vazio, parede, vazio, parede, vazio, parede, caixa, parede, vazio, parede, vazio], 
+          [parede,vazio,marca,vazio,vazio,marca,vazio,caixa,marca,vazio,vazio,marca,vazio,vazio,marca,caixa,vazio,vazio,vazio,vazio,marca,vazio,vazio,marca,vazio,vazio,parede], 
+          [vazio,parede,vazio,vazio,parede,vazio,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,vazio,parede,vazio,parede,caixa,parede,vazio,parede,caixa,vazio,parede,vazio],
+          [parede,vazio,vazio,caixa,marca,vazio,vazio,vazio,caixa,vazio,marca,vazio,vazio,marca,caixa,vazio,marca,vazio,vazio,vazio,vazio,caixa,marca,vazio,vazio,vazio,parede], 
           [vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio],
           [vazio,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,parede,vazio,vazio]
         ],
@@ -96,3 +96,4 @@ mapas([
     )
 
 ]).
+
